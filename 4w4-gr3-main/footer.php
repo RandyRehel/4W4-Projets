f87674cd781fee28b319419a8b60142fd8e46b2b
Randy Rehel <smartiesgoldgaming@gmail.com>
2022-03-17 16:49:41 -0400

    Couleurs personalisees + sections rajoutees au footer.

--- 4w4-gr3-main/footer.php
+++ 4w4-gr3-main/footer.php
@@ -1,7 +1,7 @@
 <footer class="site__footer">
-    <h2 class="footer__titre">Le beau footer</h2>
-    <p class="footer__presentation">Un site web fait par une élève du TIM à l'aide de Wordpress.</p>
-    <h3 class="footer__author">Fait par Florence Lemieux Cayer</h3>
+    <h2 class="footer__titre">The funny footer</h2>
+    <p class="footer__presentation"> Un site web fait par un élève du TIM qui aime bien le Tim hortons :). Aussi, wordpress oui, oui</p>
+    <h3 class="footer__author">Fait par l'humble Randy Rehel</h3>
 <?php 
 
 $icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
@@ -16,6 +16,9 @@ wp_nav_menu(array(
                     <div class="footer__searchform">
                     <?php get_search_form();?>
                     </div>
+                    <section class="footer__vid"></section>
+                    <section class="footer__img"></section>
+                    <section class="footer__rickroll"></section>
 </footer>
 
 </body>
