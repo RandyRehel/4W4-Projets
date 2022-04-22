11a11bc24ef4a96de91c11605424fa3d5d91096e
Randy Rehel <smartiesgoldgaming@gmail.com>
2022-04-22 15:10:57 -0400

    Javascript boite modale ouvrir et fermer

--- 4w4-gr3-exer4/javascript/boite_modale.js
+++ 4w4-gr3-exer4/javascript/boite_modale.js
@@ -1,5 +1,7 @@
 (function(){
     
+    let boite__modale = document.querySelector('.boite__modale')
+    let boite__modale__fermer = document.querySelector('.boite__modale__fermer')
 
     let cours__desc__suite = document.querySelectorAll('.cours__desc__suite')
     console.log('cours__desc__suite = ' + cours__desc__suite.length)
@@ -7,8 +9,13 @@
     for (const bouton of cours__desc__suite) {
         bouton.addEventListener('mousedown', function(){
             console.log(this.tagName)
+            boite__modale.classList.add('boite__modale__ouvrir')
+            //cours__desc__suite(boite__modale.classList)
         })
     }
 
+    boite__modale__fermer.addEventListener('mousedown', function(){
+        boite__modale.classList.remove('boite__modale__ouvrir')
+    })
 
 })()
