2f68066cec9e3e915b276b88add777713175ea15
Randy Rehel <smartiesgoldgaming@gmail.com>
2022-04-29 21:11:33 -0400

    update le front page pour inclure les menus au debut

--- Exercice3-Rehel-Randy/front-page.php
+++ Exercice3-Rehel-Randy/front-page.php
@@ -1,19 +1,22 @@
 <?php get_header() ?>
 <main class="site__main">
 
-<section class="animation">
-    <div class="animation__bloc">1</div>
-    <div class="animation__bloc">2</div>
-    <div class="animation__bloc">3</div>
-    <div class="animation__bloc">4</div>
-    <div class="animation__bloc">5</div>
-</section>
+<h2>Activités en TIM</h2>
+<?php 
+        wp_nav_menu(array("menu"=>"menu_accueil",
+                            "container"=>"nav"));
+?>
+<h2>Les événements importants pour l'année</h2>
+<?php 
+        wp_nav_menu(array("menu"=>"evenement",
+                            "container"=>"nav"));
+?>
 
-    
-   <?php if (have_posts()): while(have_posts()) : the_post(); ?>
-   <h1><?php the_title() ?></h1>
+   
+   <?php if (have_posts()): the_post(); ?>
+        <h1><?php the_title() ?></h1>
         <?php the_content() ?>   
-        <?php endwhile ?>
+  
    <?php endif ?>
    
 </main>
