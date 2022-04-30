e6e509cb44b8b9b7d85fd765e99f501351370cd8
Randy Rehel <smartiesgoldgaming@gmail.com>
2022-04-29 20:52:37 -0400

    Ajustement du front page. Enlever le ----frontpage.php--- et H1 sur titre

--- Exercice3-Rehel-Randy/front-page.php
+++ Exercice3-Rehel-Randy/front-page.php
@@ -9,9 +9,9 @@
     <div class="animation__bloc">5</div>
 </section>
 
-    <h1>---- Front-page.php ------</h1>
+    
    <?php if (have_posts()): while(have_posts()) : the_post(); ?>
-        <?php the_title() ?>
+   <h1><?php the_title() ?></h1>
         <?php the_content() ?>   
         <?php endwhile ?>
    <?php endif ?>
