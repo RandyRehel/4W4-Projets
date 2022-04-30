b9c66de6d1bef8bfc66a62fa483988ec107b260e
Randy Rehel <smartiesgoldgaming@gmail.com>
2022-04-29 22:35:01 -0400

    regler la page.php

--- Exercice3-Rehel-Randy/page.php
+++ Exercice3-Rehel-Randy/page.php
@@ -1,9 +1,9 @@
 <?php get_header() ?>
 <main class="site__main">
 
-    <h1>---- page.php ------</h1>
+    
    <?php if (have_posts()): the_post(); ?>
-        <?php the_title() ?>
+        <h1><?php the_title() ?></h1>
         <?php the_content() ?>        
    <?php endif ?>
 </main>
