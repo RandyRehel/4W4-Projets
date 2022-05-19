c7dbff1ffe464853217c32c122956e420be8f307
Randy Rehel <smartiesgoldgaming@gmail.com>
2022-05-19 14:27:41 -0400

    Ajustement des menus

--- RehelRandy_ExamFinal/front-page.php
+++ RehelRandy_ExamFinal/front-page.php
@@ -1,18 +1,20 @@
 <?php get_header() ?>
 <main class="site__main">
-<h1>Groupe #2</h1>
+<h1>Randy Rehel</h1>
 
     <h1>Les études en TIM</h1>
     
     <?php 
     /* --------------------------------------------- menu accueil */
-    wp_nav_menu(array("menu"=>"accueil",
-                            "container"=>"nav")); ?>
+    wp_nav_menu(array("menu"=>"menu_accueil",
+                              "container_class"=>"site__footer__menu",
+                              "menu_class"=>"site__footer__menu__ul")); ?>
     
    <?php 
-   /* --------------------------------------------- menu evenement */
-   wp_nav_menu(array("menu"=>"evenement",
-                            "container"=>"nav")); ?>
+   /* --------------------------------------------- menu ateliers */
+   wp_nav_menu(array("menu"=>"menu_ateliers",
+                              "container_class"=>"ateliers__menu",
+                              "menu_class"=>"ateliers__menu__ul")); ?>
 
    <?php if (have_posts()): the_post(); ?>
         <h1><?php the_title(); ?></h1>
