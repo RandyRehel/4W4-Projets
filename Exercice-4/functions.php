e0d7dcdf18103003cce6287e18e5d9324ea254f6
Randy Rehel <smartiesgoldgaming@gmail.com>
2022-05-26 18:11:34 -0400

    Caroussel Fonctionnel 100%

--- Exercice-4/functions.php
+++ Exercice-4/functions.php
@@ -9,7 +9,15 @@ function cidw_4w4_enqueue(){
       array(), '1.0.0',
       filemtime(get_template_directory() . '/javascript/boite-modale.js'),
      true );
+     wp_enqueue_script('cidw-4w4-carrousel',
+     get_template_directory_uri() . '/javascript/carrousel.js',
+      array(),
+      filemtime(get_template_directory() . '/javascript/carrousel.js'),
+     true );
 
+     if(is_front_page()){
+         wp_enqueue_script('cidw-4w4-carrousel');
+     }
 }
 
 add_action("wp_enqueue_scripts", "cidw_4w4_enqueue");
